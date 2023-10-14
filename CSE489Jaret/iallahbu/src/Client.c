668a3acde5002229f5de3c9b6964c8d8e0059015
JaretLynch <90110874+JaretLynch@users.noreply.github.com>
2023-10-14 18:39:39 -0400

    Update Client.c

--- CSE489Jaret/iallahbu/src/Client.c
+++ CSE489Jaret/iallahbu/src/Client.c
@@ -34,10 +34,6 @@ int ClientFD;
 
 
 
-
-
-
-
 int create_client_socket(int portno) {
 
 	Portno=portno;
@@ -64,11 +60,7 @@ int create_client_socket(int portno) {
 
 	}
 
-	}
-
-
-
-
+}
 
 
 
@@ -78,6 +70,8 @@ void close_connection(int client_fd) {
 
 	close(client_fd);
 
+	
+
 }
 
 
@@ -86,13 +80,11 @@ void login_to_server(const char* server_ip, int server_port) {
 
 	printf("\nLOGIN CALLED]\n");
 
-	int sockfd;
-
 	struct sockaddr_in serv_addr;
 
 
 
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+	if ((ClientFD = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 
 	
 
@@ -114,7 +106,7 @@ void login_to_server(const char* server_ip, int server_port) {
 
 		printf("ERROR: Invalid IP address.\n");
 
-		close(sockfd);
+		close(ClientFD);
 
 		return;
 
@@ -122,11 +114,11 @@ void login_to_server(const char* server_ip, int server_port) {
 
 
 
-	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+	if (connect(ClientFD, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
 
 		perror("Connect Error");
 
-		close(sockfd);
+		close(ClientFD);
 
 		return;
 
@@ -134,57 +126,41 @@ void login_to_server(const char* server_ip, int server_port) {
 
 	else{
 
-	printf("Logging in !/n");
+		printf("Logging in !/n");
 
-	ClientFD=sockfd;
+		LoggedIn=1;
 
-	LoggedIn=1;
+		char MESSAGE[10];
+
+		sprintf(MESSAGE, "%d", Portno);
+
+		printf("Sent Data===%s\n",MESSAGE);
+
+		int j=send(ClientFD,MESSAGE,strlen(MESSAGE),0);
+
+		if (j>0){
+
+			printf("DATA SENT, %d bytes",j);
+
+			}	
+
+		char *DataR = (char*) malloc(sizeof(char)*1024);
+
+		int bytes_received = recv(ClientFD, DataR, 1023,0);	
 
 
 
-        // Receive data from the server if needed.
+		while(bytes_received<=0){
 
-        char MESSAGE[10];
-
-
-
-	sprintf(MESSAGE, "%d", Portno);
-
-        printf("Sent Data===%s\n",MESSAGE);
-
-        int j=send(sockfd,MESSAGE,strlen(MESSAGE),0);
-
-        if (j>0){
-
-		printf("DATA SENT, %d bytes",j);
-
-		}
-
-		
-
-		
-
-	char *DataR = (char*) malloc(sizeof(char)*1024);
-
-	int bytes_received = recv(sockfd, DataR, 1023,0);	
-
-        
-
-	while(bytes_received<=0){
-
-		int bytes_received = recv(sockfd, DataR, 1023,0);	
+			int bytes_received = recv(ClientFD, DataR, 1023,0);	
 
 		}
 
-	DataR[bytes_received] = '\0';
+		DataR[bytes_received] = '\0';
 
-        printf("Received from server: %s\n", DataR);
+		printf("Received from server: %s\n", DataR);
 
-	// Send some message to server indicating login (depends on your protocol
-
-	
-
-process_client_commands();
+		process_client_commands();
 
 	}
 
@@ -194,57 +170,51 @@ process_client_commands();
 
 }
 
-    void process_client_commands() {
-
-    
 
 
+void process_client_commands() {
 
-    // The loop to keep client running and accept commands
+	// The loop to keep client running and accept commands
 
-    while(1) {
+	while(1) {
 
-        printf("[PA1-Client@CSE489/589]$ \n");
-
-	char *msg = (char*) malloc(sizeof(char)*256);
+		printf("[PA1-Client@CSE489/589]$ \n");
 
 
 
+		char *msg = (char*) malloc(sizeof(char)*256);
+
+		char *login = (char*) malloc(sizeof(char)*6);		
+
+		strncpy(login,msg,5);
+
+		login[5] = '\0' ;
 
 
 
+		fgets(msg, 256, stdin); 
 
-	fgets(msg, 256, stdin); // read from stdin
+		msg[strlen(msg)-1]= '\0';
 
-	msg[strlen(msg)-1]= '\0';
+		if (strcmp(msg, "AUTHOR") == 0) {
 
-	printf("I got: %s(size:%lu chars)\n", msg, strlen(msg));
+			handle_author_command();
 
-        if (strcmp(msg, "AUTHOR") == 0) {
+		} 
 
-            handle_author_command();
+		else if (strcmp(msg, "IP") == 0) {
 
-        } 
+			handle_ip_command();
 
-        else if (strcmp(msg, "IP") == 0) {
+		} 
 
-            handle_ip_command();
-
-            free(msg);
-
-        } 
-
-        
-
-        if (strcmp(msg,"PORT")==0){
+		else if (strcmp(msg,"PORT")==0){
 
 			handle_port_command(Portno);
 
-			free(msg);
-
 		}
 
-	if (strcmp(msg,"EXIT")==0){
+		else if (strcmp(msg,"EXIT")==0){
 
 			printf("TRYING TO CLOSE");
 
@@ -254,126 +224,108 @@ process_client_commands();
 
 		}
 
-	if ((strcmp(msg,"REFRESH")==0) && (LoggedIn==1)){
+		else if ((strcmp(msg,"REFRESH")==0) && (LoggedIn==1)){
 
-        	printf("Sent Data===%s\n",msg);
+			int j=send(ClientFD,msg,strlen(msg),0);
 
-        	int j=send(ClientFD,msg,strlen(msg),0);
+			char *DataR = (char*) malloc(sizeof(char)*1024);
 
-        	char *DataR = (char*) malloc(sizeof(char)*1024);
+			int bytes_received = recv(ClientFD, DataR, 1023,0);	
 
-		int bytes_received = recv(ClientFD, DataR, 1023,0);	
 
-		
 
-		while(bytes_received<=0){
+			while(bytes_received<=0){
 
-			int bytes_received = recv(ClientFD, DataR, strlen(DataR),0);	
+				int bytes_received = recv(ClientFD, DataR, strlen(DataR),0);	
 
 			}
 
-		DataR[bytes_received] = '\0';
+			DataR[bytes_received] = '\0';
 
-        	printf("Received from server: %s\n", DataR);
+			printf("%s\n", DataR);
 
-        	free(msg);
-
-	
-
-	}
-
-	char *login = (char*) malloc(sizeof(char)*6);		
-
-	strncpy(login,msg,5);
-
-	login[5] = '\0' ;
-
-	
-
-	if ((strcmp(login,"LOGIN")==0) && (LoggedIn==0)){
-
-		char *Cport;
-
-		char *IP;
-
-		Cport = (char*) malloc(30*sizeof(char));
-
-		IP = (char*) malloc(30*sizeof(char));
-
-		printf("LOGIN detected");
-
-		int count=0;
-
-		int iterator=0;
-
-		int iterator2=0;
-
-		for (int i=0; i<strlen(msg); i++){
-
-		
-
-			printf("%c\n",msg[i]);
-
-			char Character[1];
-
-			strncpy(Character,&msg[i],1);
-
-			Character[1]='\0';
-
-			
-
-			if(count==1){
-
-			
-
-				IP[iterator2]=*Character;
-
-				iterator2 ++;
-
-			}
-
-			
-
-			
-
-			if(count==2){
-
-				
-
-				Cport[iterator]=*Character;
-
-				printf("Character is %c",*Character);
-
-				iterator ++;
-
-			}
-
-			if (strcmp(Character," ")==0){
-
-				count++;
-
-				printf("DETECTED\n");
-
-			}
-
-	}
-
-		IP[strlen(IP)-1]= '\0';
-
-		int IPlen= strlen(IP);
-
-		
-
-		int Portlen= strlen(Cport);
-
-		int PORTN= atoi(Cport);
-
-		login_to_server(IP,PORTN);
-
-		free(msg);
+			free(msg);
 
 		}
 
+
+
+		else if ((strcmp(login,"LOGIN")==0) && (LoggedIn==0)){
+
+			char *Client_Port = (char*) malloc(30*sizeof(char));
+
+			char *Client_IP = (char*) malloc(30*sizeof(char));
+
+			printf("LOGIN detected");
+
+			int count=0;
+
+			int iterator=0;
+
+			int iterator2=0;
+
+			
+
+			for (int i=0; i<strlen(msg); i++){
+
+
+
+				printf("%c\n",msg[i]);
+
+				char Character[1];
+
+				strncpy(Character,&msg[i],1);
+
+				Character[1]='\0';
+
+
+
+				if(count==1){
+
+					Client_IP[iterator2]=*Character;
+
+					iterator2 ++;
+
+				}
+
+				if(count==2){
+
+					Client_Port[iterator]=*Character;
+
+					iterator ++;
+
+				}
+
+				if (strcmp(Character," ")==0){
+
+					count++;
+
+				}
+
 			}
+
+			
+
+			Client_IP[strlen(Client_IP)-1]= '\0';
+
+			int IPlen= strlen(Client_IP);
+
+			int Portlen= strlen(Client_Port);
+
+			int PORTN= atoi(Client_Port);
+
+			login_to_server(Client_IP,PORTN);
+
+			free(msg);
+
+		}
+
+		else{
+
+		printf("[%s:ERROR]\n",msg);
+
+		}
+
+	}
 
 }
